1529475fe4c8dfdb15d4020f2e72b67ecd1e4f6a
Rithul Kamesh <hi@rithul.dev>
2022-11-04 21:39:58 +0530

    Prisma Setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <>
+      Hello, World;
+    </>
+  )
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/components/**/*.{js,ts,tsx,jsx}",
+    "./src/layouts/**/*.{js,ts,tsx,jsx}",
+    "./src/pages/*.{js,ts,tsx,jsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
